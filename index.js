3007c20a237a05fc5095a5898b491fa5124bf5e3
mdsadikul <mdsadikulislam891@gmail.com>
2023-03-05 07:59:24 +0600

    node.js server create

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const {name, product} = require("./other");
 
 console.log(name, product);
+
+
+// node.js diye Server create.
+const http = require("http");
+
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+    res.end("Hello Programmer");
+});
+
+server.listen(PORT);
+
+console.log(`Server is running on ${PORT}`);
