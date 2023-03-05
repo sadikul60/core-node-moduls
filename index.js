5db0c8582d05af3a91334517e31d62a57513cbeb
mdsadikul <mdsadikulislam891@gmail.com>
2023-03-05 08:32:48 +0600

    added node route page

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ const http = require("http");
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-    res.end("Hello Programmer");
+    if(req.url=="/"){
+        res.writeHead(200, {"content-type" : "text/html"});
+        res.write('<p> This is Home page </p>');
+        res.end()
+    }
+
+    else if(req.url=="/contact"){
+        res.writeHead(200, {"content-type" : "text/html"});
+        res.write('<p> This is Contact page </p>');
+        res.end()
+    }
+
+    else if(req.url=="/aboutUs"){
+        res.writeHead(200, {"content-type" : "text/html"});
+        res.write('<p> This is About Us page </p>');
+        res.end()
+    };
 });
 
 server.listen(PORT);
